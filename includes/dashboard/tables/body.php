092c3d127c7a8a2302aaef42cd9c19ffcf511adf
kirano <KIERAN.ONG.ZHI@student.mmu.edu.my>
2026-04-11 02:28:03 +0800

    v0.0.5

--- includes/dashboard/tables/body.php
+++ includes/dashboard/tables/body.php
@@ -85,10 +85,10 @@
                             class="border-b border-slate-200 bg-slate-100 text-sm font-medium text-slate-600 dark:bg-surface-dark">
                             <tr>
                                 <th class="px-2.5 py-2 text-start font-medium">
-                                    Transaction
+                                    Table Code
                                 </th>
                                 <th class="px-2.5 py-2 text-start font-medium">
-                                    Amount
+                                    Available Seat
                                 </th>
                                 <th class="px-2.5 py-2 text-start font-medium">
                                     Date
@@ -97,7 +97,7 @@
                                     Status
                                 </th>
                                 <th class="px-2.5 py-2 text-start font-medium">
-                                    Account
+                                    Branch
                                 </th>
                                 <th class="px-2.5 py-2 text-start font-medium">
                                 </th>
