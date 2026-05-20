600a4a6440a22f3d1f2d0e8ad4bca11f74eca709
kirano <KIERAN.ONG.ZHI@student.mmu.edu.my>
2026-05-20 09:37:55 +0800

    v0.0.26

---
+++ hotel-booking-records.cpp
@@ -0,0 +1,33 @@
+// IF YOU SEE IT, JUST IGNORE THIS FILE AND THE CODE
+// WE'RE DOING ANOTHER ASSIGNMENT AND PROJECT IN THIS FILE
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+struct Booking
+{
+    int bookingId;
+    string customerName;
+    double price;
+    int checkedInDay;
+    /*string checkedInDate;
+    string checkedOutDate;*/
+};
+
+int main()
+{
+    /*cout << "Please enter student's ID and marks" << endl;
+
+    student *ptr = new student[5];
+
+    for (int i=0; i < 5; i++)
+    {
+        cout << "\tStudent " << (i+1) << " ID : ";
+        cin >> (*ptr).studentId;
+        cout << "\tStudent " << (i+1) << " mark : ";
+        cin >> (*ptr).mark;
+    }*/
+
+    return 0;
+}
